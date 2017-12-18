ab1784d5cf7c09283b191ce51347617fffa59486
Irene DeVera <irenneeyyy@gmail.com>
2017-12-18 15:53:44 -0800

    Bundle installed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
-
-
 end
+
+
+# Prefix Verb URI Pattern                         Controller#Action
+# customers GET  /customers(.:format)                customers#index
+# movies GET  /movies(.:format)                   movies#index
+#  movie GET  /movies/:title(.:format)            movies#show
+# check_out POST /rentals/:title/check-out(.:format) rentals#check_out
+# check_in POST /rentals/:title/return(.:format)    rentals#check_in
+# overdue GET  /rentals/overdue(.:format)          rentals#overdue
